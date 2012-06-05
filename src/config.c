b3d678c5f2f2f6208f37780090427b6c47e446e4
Matthias Schiffer <mschiffer@universe-factory.net>
2012-06-05 22:32:19 +0200

    Increase reorder count

--- src/config.c
+++ src/config.c
@@ -60,7 +60,7 @@ static void default_config(fastd_config *conf) {
 	conf->peer_stale_time = 90;
 	conf->eth_addr_stale_time = 300;
 
-	conf->reorder_count = 32;
+	conf->reorder_count = 64;
 	conf->reorder_time = 10;
 
 	conf->ifname = NULL;
