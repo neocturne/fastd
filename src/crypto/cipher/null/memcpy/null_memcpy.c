d646bd03321217d0af8d4787f8aa2a044c2a63d1
Matthias Schiffer <mschiffer@universe-factory.net>
2013-11-17 21:13:55 +0100

    Add null cipher

---
+++ src/crypto/cipher/null/memcpy/null_memcpy.c
@@ -0,0 +1,71 @@
+/*
+  Copyright (c) 2012-2013, Matthias Schiffer <mschiffer@universe-factory.net>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include "../../../../fastd.h"
+
+
+static fastd_cipher_context_t* null_initialize(fastd_context_t *ctx UNUSED) {
+	return NULL;
+}
+
+static size_t null_key_length(fastd_context_t *ctx UNUSED, const fastd_cipher_context_t *cctx UNUSED) {
+	return 0;
+}
+
+
+static fastd_cipher_state_t* null_init_state(fastd_context_t *ctx UNUSED, const fastd_cipher_context_t *cctx UNUSED, const uint8_t *key UNUSED) {
+	return NULL;
+}
+
+static size_t null_iv_length(fastd_context_t *ctx UNUSED, const fastd_cipher_state_t *state UNUSED) {
+	return 0;
+}
+
+static bool null_memcpy(fastd_context_t *ctx UNUSED, const fastd_cipher_state_t *state UNUSED, fastd_block128_t *out, const fastd_block128_t *in, size_t len, const uint8_t *iv UNUSED) {
+	memcpy(out, in, len);
+	return true;
+}
+
+static void null_free_state(fastd_context_t *ctx UNUSED, fastd_cipher_state_t *state UNUSED) {
+}
+
+static void null_free(fastd_context_t *ctx UNUSED, fastd_cipher_context_t *cctx UNUSED) {
+}
+
+const fastd_cipher_t fastd_cipher_null_memcpy = {
+	.name = "memcpy",
+
+	.initialize = null_initialize,
+
+	.key_length = null_key_length,
+	.init_state = null_init_state,
+
+	.iv_length = null_iv_length,
+	.crypt = null_memcpy,
+
+	.free_state = null_free_state,
+	.free = null_free,
+};
