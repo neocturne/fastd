ad22fc93f71642594b8c5939fe80d2720c4db9c7
Matthias Schiffer <mschiffer@universe-factory.net>
2020-05-22 20:34:37 +0200

    Development version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ copyright = '2012-2020, Matthias Schiffer'
 # built documents.
 #
 # The short X.Y version.
-version = '19'
+version = '19+'
 # The full version, including alpha/beta/rc tags.
 release = version
 
