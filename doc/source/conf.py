2ce6095b2795052e34110599c484205468fb9fa6
Matthias Schiffer <mschiffer@universe-factory.net>
2020-10-19 21:24:26 +0200

    fastd v21

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ copyright = '2012-2020, Matthias Schiffer'
 # built documents.
 #
 # The short X.Y version.
-version = '20+'
+version = '21'
 # The full version, including alpha/beta/rc tags.
 release = version
 
