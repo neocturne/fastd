7dc53ab69e494b9bfb982f729d9f2c510b3629ec
Matthias Schiffer <mschiffer@universe-factory.net>
2021-03-12 19:34:45 +0100

    Optionally link libmnl into fastd statically
    Regular builds should just link libmnl dynamically, but for OpenWrt and
    similar embedded distros this allows to reduce the required flash size a
    bit.

---
+++ src/dep/libmnl/src/internal.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+  Copyright (c) 2021, Matthias Schiffer <mschiffer@universe-factory.net>
+  All rights reserved.
+*/
+
+#pragma once
+
+#define EXPORT_SYMBOL(x)
