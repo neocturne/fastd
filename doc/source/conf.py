e0d59850b680e8fd9d39bf1edff2062cc5e27c6f
Matthias Schiffer <mschiffer@universe-factory.net>
2024-12-30 02:24:17 +0100

    fastd v23

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,7 +54,7 @@ copyright = 'Matthias Schiffer'
 # built documents.
 #
 # The short X.Y version.
-version = '22+'
+version = '23'
 # The full version, including alpha/beta/rc tags.
 release = version
 
